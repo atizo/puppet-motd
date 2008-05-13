1d778575ccd90bc3d1ef717b92eb51da1f079de6
Marcel Haerry <haerry@puzzle.ch>
2008-05-13 19:59:13 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# motd module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "motd": }
+class motd {}
