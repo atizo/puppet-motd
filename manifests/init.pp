c57ac23145cec9b4ac99f4bfc07908a8dcfbbef4
Marcel Haerry <haerry@puzzle.ch>
2008-05-13 20:52:03 +0200

    added classes

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,28 @@
 # motd module
 # Puzzle ITC - haerry+puppet(at)puzzle.ch
 # GPLv3
+# include motd::puppetmaster on the 
+# master before you to any motd::client
+# calls. 
+# content and generate functions
+# are evaluated on the master!
+# you can pass additional messages by
+# setting $motd_message to some string
 #######################################
 
 
 # modules_dir { "motd": }
-class motd {}
+class motd { }
+
+class motd::client {
+    file{"/etc/motd":
+        content => generate("figlet ${hostname}; echo -e ${motd_message}),
+        owner => root, group => 0, mode => 0644;
+    } 
+}
+
+class motd::puppetmaster {
+    package{figlet:
+        ensure => installed,
+    }
+}
