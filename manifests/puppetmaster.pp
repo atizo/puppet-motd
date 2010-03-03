31af20140a5b5a3f12b26648e66c98abf783e5e1
Simon Josi <josi@puzzle.ch>
2010-03-03 17:56:21 +0100

    reintroduce dependency and make sure the master includes also the client

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,10 +1,13 @@
 class motd::puppetmaster {
+    include motd
+
     package{'figlet':
         ensure => present,
     }
     file{"/usr/local/sbin/motd_gen.sh":
         source => "puppet://$server/modules/motd/motd_gen.sh",
-        require => Package['figlet'], 
+        require => Package['figlet'],
+        before => File['/etc/motd'],
         owner => puppet, group => 0, mode => 0755;
     }
 }
