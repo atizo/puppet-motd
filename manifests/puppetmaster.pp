150430d14765ad67baeb862e1300d72ef5f8d26b
Simon Josi <josi@puzzle.ch>
2010-03-03 17:51:19 +0100

    remove dependency that is only really helpful on the master itself

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,11 +1,9 @@
 class motd::puppetmaster {
     package{'figlet':
         ensure => present,
-        before => File['/etc/motd'],
     }
     file{"/usr/local/sbin/motd_gen.sh":
-        source => "puppet://$server/motd/motd_gen.sh",
-        before => File['/etc/motd'],
+        source => "puppet://$server/modules/motd/motd_gen.sh",
         require => Package['figlet'], 
         owner => puppet, group => 0, mode => 0755;
     }
