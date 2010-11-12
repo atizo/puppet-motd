6d4a4de9c9e350abcbf5b2702a5a285f1bc851aa
Simon Josi <simon.josi@atizo.com>
2010-11-12 21:32:27 +0100

    add new copyright notice; switch to two space indentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,26 +2,25 @@
 # motd module
 #
 # Copyright 2008, Puzzle ITC GmbH
+# Copyright 2010, Atizo AG
 # Marcel Härry haerry+puppet(at)puzzle.ch
-# Simon Josi josi+puppet(at)puzzle.ch
+# Simon Josi simon.josi+puppet(at)atizo.com
 #
 # This program is free software; you can redistribute 
 # it and/or modify it under the terms of the GNU 
 # General Public License version 3 as published by 
 # the Free Software Foundation.
 #
-# include motd::puppetmaster on the 
-# master before you do any motd::client
-# calls. 
+# include motd::puppetmaster on the master 
+# before you do any motd::client calls. 
 # content and generate functions
 # are evaluated on the master!
 # you can pass additional messages by
 # setting $motd_message to some string
-#
 
 class motd {
-    file{'/etc/motd':
-        content => generate('/usr/local/sbin/motd_gen.sh', $hostname, $motd_message),
-        owner => root, group => 0, mode => 0644;
-    }
+  file{'/etc/motd':
+    content => generate('/usr/local/sbin/motd_gen.sh', $hostname, $motd_message),
+    owner => root, group => 0, mode => 0644;
+   }
 }
--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,13 +1,12 @@
 class motd::puppetmaster {
-    include motd
-
-    package{'figlet':
-        ensure => present,
-    }
-    file{"/usr/local/sbin/motd_gen.sh":
-        source => "puppet://$server/modules/motd/motd_gen.sh",
-        require => Package['figlet'],
-        before => File['/etc/motd'],
-        owner => puppet, group => 0, mode => 0755;
-    }
+  include motd
+  package{'figlet':
+    ensure => present,
+  }
+  file{"/usr/local/sbin/motd_gen.sh":
+    source => "puppet://$server/modules/motd/motd_gen.sh",
+    require => Package['figlet'],
+    before => File['/etc/motd'],
+    owner => puppet, group => 0, mode => 0755;
+  }
 }
