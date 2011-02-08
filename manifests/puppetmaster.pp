ef0fe607c38f966fd63032ea0ea584d87c65d58b
Simon Josi <simon.josi@atizo.com>
2011-02-08 12:49:10 +0100

    puppetmaster doesn't depend on /etc/motd

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -5,7 +5,6 @@ class motd::puppetmaster {
   file{"/usr/local/sbin/motd_gen.sh":
     source => "puppet://$server/modules/motd/motd_gen.sh",
     require => Package['figlet'],
-    before => File['/etc/motd'],
     owner => puppet, group => 0, mode => 0755;
   }
 }
